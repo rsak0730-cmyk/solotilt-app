aa3a6ad13fdb1731ea796103d7faad42ff1e82e9
rsak0730-cmyk <321288827+rsak0730-cmyk@users.noreply.github.com>
2026-09-23 09:18:56 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'tilt_home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const SoloTiltApp());
+}
+
+class SoloTiltApp extends StatelessWidget {
+  const SoloTiltApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SoloTilt',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const TiltHomeScreen(),
+    );
+  }
+}
